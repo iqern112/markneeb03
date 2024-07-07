dd2a1d75b655f4eef58cffc1309649ef1e98e57a
First1150 <first310405@gmail.com>
2024-07-07 16:07:03 +0700

    asd

--- server.js
+++ server.js
@@ -14,25 +14,52 @@ const roomNames = [123]; // เก็บเฉพาะชื่อห้อง
 app.use(express.static(path.join(__dirname, 'client')));
 
 io.on('connection', (socket) => {
-    
+    let userId;
     socket.on('gust', () => {
-        const userId = socket.id;
+        userId = socket.id;
         onlineUsers.add(userId);
         // ส่งรายชื่อผู้ใช้ที่ออนไลน์ให้กับผู้ใช้ทั้งหมดที่เข้าเว็บไซต์
     io.emit('update-online-users', Array.from(onlineUsers));
         console.log(`new user : ${userId}`);
         console.log("gust")
+        localStorage.setItem('userId', userId); // หรือ sessionStorage.setItem('userId', userId);
         socket.emit('gustOk');
-        
+        // เมื่อเชื่อมต่อ
+    socket.on('get-all-rooms', () => {
+        socket.emit('all-rooms', roomNames); // ส่งข้อมูลห้องทั้งหมดกลับไปยัง client
     });
-    
+    });
+    socket.on('join-room', (roomId) => {
+        socket.join(roomId);
+        if (!rooms.has(roomId)) {
+            rooms.set(roomId, new Set());
+        }
+        rooms.get(roomId).add(userId);
+        socket.to(roomId).emit('chat-message', { userId: 'system', msg: `User ${userId} has joined the room.` });
+        io.to(socket.id).emit('join-room');
+    });
+    socket.on('create-room', (roomName) => {
+        const roomId = uuidv4();
+        rooms.set(roomId,{ roomName });
+        roomNames.push(roomName); // เพิ่มชื่อห้องใหม่ลงใน Array
+        io.emit('room-created', { roomId, roomName });
+    });
+    socket.on('chat-message', (roomId, userId, msg) => {
+        socket.to(roomId).emit('chat-message', { userId, msg });
+    });
     socket.on('disconnect', () => {
         // ลบผู้ใช้ออกจากรายการของผู้ใช้ที่ออนไลน์
        onlineUsers.delete(userId);
        // ส่งรายชื่อผู้ใช้ที่ออนไลน์ให้กับผู้ใช้ทั้งหมดที่เข้าเว็บไซต์
        io.emit('update-online-users', Array.from(onlineUsers));
+       console.log(`disconnect user : ${userId}`);
        
-       
+       rooms.forEach((users, roomId) => {
+           if (users.has(socket.userId)) {
+               users.delete(socket.userId);
+               socket.to(roomId).emit('chat-message', { userId: 'system', msg: `User ${socket.userId} has left the room.` });
+           }
+       });
    });
 });
 
