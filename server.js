531723266362febfc1c37c2d564979ae31bbe1af
iqern112 <sernsan89@gmail.com>
2024-07-08 22:23:33 +0700

    add online player list

--- server.js
+++ server.js
@@ -16,24 +16,49 @@ app.use(session({
     saveUninitialized: true
 }));
 
+const connectedUsers = {};
+
 io.on('connection', (socket) => {
-    
+
+    // if (!socket.handshake.session.userId) {
+    //     socket.handshake.session.userId = socket.id;
+    // }
+    // const userId = socket.handshake.session.userId;
+    // connectedUsers[userId] = socket.id;
+
     socket.on('gust', () => {
         console.log("gust");
         socket.emit('gustOk');
     });
 
-    socket.on('in-to-lobby',()=>{
-        if(!session.userId){
-            session.userId = socket.id;
-        }
-        socket.emit('your-user-id', session.userId);
+    socket.on('req-user-id',()=>{
+        // if(!session.userId){
+        //     session.userId = socket.id;
+        // }
+        let userId = socket.id;
+        connectedUsers[userId] = userId;
+        socket.emit('res-user-id', userId);
+        io.emit('res-online-users', Object.keys(connectedUsers));
+        console.log(`user ${userId} connected`)
+        io.emit('res-online-users', Object.keys(connectedUsers));
+    });
+
+
+    socket.on('req-online-users',()=>{
+        socket.emit('res-online-users', Object.keys(connectedUsers));
     });
 
     socket.on('new-room',()=>{
         console.log("new room");
         socket.emit('acess-room');
     });
+
+    // socket.on('disconnect', () => {
+    //     delete connectedUsers[userId];
+    //     io.emit('res-online-users', Object.keys(connectedUsers));
+    //     // console.log(`User ${userId} disconnected`);
+    // });
+
 });
 
 const port = process.env.port || 3000;
